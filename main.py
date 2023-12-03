8306ba6f7927e9e7e59de8f679ef7b33699edf34
Kushagra <kushagragupta97@gmail.com>
2023-12-03 15:26:33 -0500

    Include sentiment analysis API

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import pickle
+import nltk
 from fastapi import FastAPI
 app=FastAPI()
 
 @app.post("/")
 def get_name(text='Sample text'):
     return {'Name':text}
+
+with open('sentiment_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.post("/analyse/sentiment")
+def analyse_sentiment(text: str):
+    return model.polarity_scores(text)
